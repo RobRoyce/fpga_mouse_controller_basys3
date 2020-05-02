884a8c66e015f87e8498fac1973b606a51036cb5
Rob Royce <robertris2@gmail.com>
2020-05-01 18:02:55 -0700

    Added x and y overflow signals

--- ps2_signal_map.v
+++ ps2_signal_map.v
@@ -1,8 +1,5 @@
 `timescale 1ns / 1ps
 
-// This module takes the 8-bit words (after they have been validated) and
-// converts them into coherent signals that can be operated on.
-
 module ps2_mouse_map(
                      input wire        i_clk,
                      input wire        i_reset,
@@ -15,11 +12,20 @@ module ps2_mouse_map(
                      output wire       o_x_sign,
                      output wire       o_y_sign,
                      output wire       o_l_click,
-                     output wire       o_r_click
+                     output wire       o_r_click,
+                     output wire       o_x_overflow,
+                     output wire       o_y_overflow
                      );
+   // This module takes the 8-bit words (after they have been validated) and
+   // converts them into cohesive signals that can be operated on. Output names
+   // are indicative of the mappings. Note that other signals of importance may
+   // not be accounted for here. Namely, the Microsoft Intellimouse type
+   // extension for supporting a scroll wheel mouse. This may be added in the
+   // future (feel free to open a PR).
 
    assign o_x = i_signal2;
    assign o_y = i_signal3;
+   assign {o_x_overflow, o_y_overflow} = i_signal1[1:0];
    assign {o_x_sign, o_y_sign} = i_signal1[3:2];
    assign {o_l_click, o_r_click} = i_signal1[7:6];
 endmodule
